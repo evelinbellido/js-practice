883d92ca2d06489367de925ad6d44b64d9411544
Evelin <bellidoevelin@gmail.com>
2022-09-13 16:48:49 -0500

    Create swicht.js
    este es un caso donde compara 2 valores con un id dentro de un swicht los valida con un boolean.

---
+++ swicht.js
@@ -0,0 +1,14 @@
+var val1 = prompt("ingrese su primer valor");
+var val2 = prompt("ingreso su segundo valor");
+
+    switch (true) {
+        case (val1 > val2):
+            console.log("VALOR 1 MAYOE Q 2");
+            break;
+            
+        case ( val2 > val1):
+            console.log("VALOR 2 MAYOR QUE 1");
+            break;
+        default:
+            break;
+}
